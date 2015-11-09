0e459f4f8916ffad1c61bceffbbddc842893d08a
Michael Richardson <michaelrichardson@Severus.local>
2015-11-09 15:47:15 -0800

    Reorganized test_infection.py

--- test_infection.py
+++ test_infection.py
@@ -4,124 +4,149 @@ import unittest
 from infection import total_infection
 from infection import limited_infection
 
+def generate_test_file(dir, file_name=None, num_users=10000, version='1.1',
+                        user_to_coach_ratio=None, school_size=None,
+                        uncoached_to_coached_ratio=1, **kwargs):
+
+    ids = ['id{:013d}'.format(i) for i in xrange(num_users)]
+    test_file_dir = dir
+    if not os.path.isdir(test_file_dir):
+        os.mkdir(test_file_dir)
+    out_file_name = os.path.join(test_file_dir, file_name)
+    out_file = open(out_file_name, 'w')
+
+    if school_size and user_to_coach_ratio:
+        for i, id in enumerate(ids):
+            if i % user_to_coach_ratio == 0:
+                lower = (i // school_size) * school_size + 1
+                upper = lower + school_size
+                offset = (i % school_size) // user_to_coach_ratio
+                adj_list = ids[lower + offset:upper:uncoached_to_coached_ratio]
+                out_file.write(id + '\t' + version + '\t' + ','.join(adj_list) + '\n')
+            else:
+                out_file.write(id + '\t' + version + '\t' + '\n')
+
+    elif user_to_coach_ratio:
+        for i, id in enumerate(ids):
+            if i % user_to_coach_ratio == 0:
+                lower = (i // user_to_coach_ratio) * user_to_coach_ratio + 1
+                upper = lower + user_to_coach_ratio - 1
+                adj_list = ids[lower:upper:uncoached_to_coached_ratio]
+                out_file.write(id + '\t' + version + '\t' + ','.join(adj_list) + '\n')
+            else:
+                out_file.write(id + '\t' + version + '\t' + '\n')
+    else:
+        for i, id in enumerate(ids):
+            out_file.write(id + '\t' + version + '\t' + '\n')
+
+    out_file.close()
+
 
 class InfectionTestCase(unittest.TestCase):
     """Tests for `infection.py`."""
 
     test_dir = 'test_files'
-    test_file_list = None
+    test_file_configs = []
 
     @classmethod
     def setUpClass(cls):
-        dir = 'test_files'
-        test_files_info = cls.generate_test_files(cls.test_dir, num_nodes=10000, version='1.1',
-                            node_with_edges_frequency=100, group_size=2000)
-        cls.test_file_list = sorted(test_files_info.keys())
+
+        # Create a graph file where every hundredth person is a coach and coaches
+        # half of the following 99 users.
+
+        test_file_config = {
+            'file_name': 'input_01.txt',
+            'version': '1.1',
+            'num_users': 10000,
+            'user_to_coach_ratio': 100,
+            'school_size': None,
+            'uncoached_to_coached_ratio': 2,
+            'expected_test_total_infection': 51,
+            'expected_test_limited_infection_percentage_10': 1000,
+            'expected_test_limited_infection_amount_2019_tolerance_1_percent': 2019
+        }
+        cls.test_file_configs.append(test_file_config)
+        generate_test_file(cls.test_dir, **test_file_config)
+
+        # Create a graph file with containing no edges
+
+        test_file_config = {
+            'file_name': 'input_03.txt',
+            'version': '1.1',
+            'num_users': 10000,
+            'user_to_coach_ratio': None,
+            'school_size': None,
+            'uncoached_to_coached_ratio': None,
+            'expected_test_total_infection': 1,
+            'expected_test_limited_infection_percentage_10': 1000,
+            'expected_test_limited_infection_amount_2019_tolerance_1_percent': 2019
+        }
+        cls.test_file_configs.append(test_file_config)
+        generate_test_file(cls.test_dir, **test_file_config)
+
+        # Create a graph file consisting of a single connected component
+
+        test_file_config = {
+            'file_name': 'input_02.txt',
+            'version': '1.1',
+            'num_users': 10000,
+            'user_to_coach_ratio': 100,
+            'school_size': 10000,
+            'uncoached_to_coached_ratio': 2,
+            'expected_test_total_infection': 10000,
+            'expected_test_limited_infection_percentage_10': 0,
+            'expected_test_limited_infection_amount_2019_tolerance_1_percent': 0
+        }
+        cls.test_file_configs.append(test_file_config)
+        generate_test_file(cls.test_dir, **test_file_config)
+
+        # Create a graph where edges occur only within groups of 2000 users
+
+        test_file_config = {
+            'file_name': 'input_04.txt',
+            'version': '1.1',
+            'num_users': 10000,
+            'user_to_coach_ratio': 100,
+            'school_size': 2000,
+            'uncoached_to_coached_ratio': 20,
+            'expected_test_total_infection': 101,
+            'expected_test_limited_infection_percentage_10': 0,
+            'expected_test_limited_infection_amount_2019_tolerance_1_percent': 2000
+        }
+
+        cls.test_file_configs.append(test_file_config)
+        generate_test_file(cls.test_dir, **test_file_config)
+
 
     @classmethod
     def tearDownClass(cls):
-        for file_name in cls.test_file_list:
-            os.remove(file_name)
+        for config in cls.test_file_configs:
+            os.remove(os.path.join(cls.test_dir, config['file_name']))
         if not os.listdir(cls.test_dir):
             os.rmdir(cls.test_dir)
 
     def test_total_infection(self):
-        correct_num_affected_users = [51, 1, 10000, 101]
-        for i, file_name in enumerate(self.test_file_list):
+        for config in self.test_file_configs:
+            file_name = os.path.join(self.test_dir, config['file_name'])
             affected_users = total_infection(file_name, 'id0000000000001', '1.17')
-            self.assertEqual(affected_users, correct_num_affected_users[i])
+            expected = config['expected_test_total_infection']
+            self.assertEqual(affected_users, expected)
 
     def test_limited_infection_percentage_10(self):
-        correct_num_affected_users = [1000, 1000, 0, 0]
-        for i, file_name in enumerate(self.test_file_list):
+        for config in self.test_file_configs:
+            file_name = os.path.join(self.test_dir, config['file_name'])
             affected_users = limited_infection(file_name, '1.34', percentage=0.1, 
                                              amount=None, tolerance=0, userid=None)
-            self.assertEqual(affected_users, correct_num_affected_users[i])
+            expected = config['expected_test_limited_infection_percentage_10']
+            self.assertEqual(affected_users, expected)
 
     def test_limited_infection_amount_2019_tolerance_1_percent(self):
-        correct_num_affected_users = [2019, 2019, 0, 2000]
-        for i, file_name in enumerate(self.test_file_list):
+        for config in self.test_file_configs:
+            file_name = os.path.join(self.test_dir, config['file_name'])
             affected_users = limited_infection(file_name, '1.34', percentage=None, 
                                              amount=2019, tolerance=0.01, userid=None)
-            self.assertEqual(affected_users, correct_num_affected_users[i])
-    
-    @staticmethod
-    def generate_test_files(dir, num_nodes=10000, version='1.1',
-                            node_with_edges_frequency=100, group_size=2000):
-
-        ids = ['id{:013d}'.format(i) for i in xrange(num_nodes)]
-        test_files_path = dir
-        if not os.path.isdir(test_files_path):
-            os.mkdir(test_files_path)
-
-        info_test_files = {}
-
-        # Create a graph file where every `node_with_edges_frequency` node is connected to half
-        # of the `node_with_edges_frequency - 1` following nodes. Each connected component will 
-        # have `node_with_edges_frequency / 2 + 1` nodes.
-        out_file_name = os.path.join(test_files_path, 'test_input_01.txt')
-        out_file = open(out_file_name, 'w')
-        info_test_files[out_file_name] = {}
-        info_test_files[out_file_name]['num_nodes'] = len(ids)
-        info_test_files[out_file_name]['num_edges'] = 0
-        for i, id in enumerate(ids):
-            if i % node_with_edges_frequency == 0:
-                lower = (i // node_with_edges_frequency) * node_with_edges_frequency + 1
-                upper = lower + node_with_edges_frequency - 1
-                adj_list = ids[lower:upper:2]
-                info_test_files[out_file_name]['num_edges'] += len(adj_list)
-            else:
-                adj_list = []
-            out_file.write(id + '\t' + version + '\t' + ','.join(adj_list) + '\n')
-        out_file.close()
-
-        # Create a graph file consisting of only nodes (no edges)
-        out_file_name = os.path.join(test_files_path, 'test_input_02.txt')
-        out_file = open(out_file_name, 'w')
-        info_test_files[out_file_name] = {}
-        info_test_files[out_file_name]['num_nodes'] = len(ids)
-        info_test_files[out_file_name]['num_edges'] = 0
-        for id in ids:
-            out_file.write(id + '\t' + version + '\t' + '\n')
-        out_file.close()
-
-        # Create a graph file consisting of a single connected component
-        out_file_name = os.path.join(test_files_path, 'test_input_03.txt')
-        out_file = open(out_file_name, 'w')
-        info_test_files[out_file_name] = {}
-        info_test_files[out_file_name]['num_nodes'] = len(ids)
-        info_test_files[out_file_name]['num_edges'] = 0
-        for i, id in enumerate(ids):
-            if i % node_with_edges_frequency == 0:
-                lower = (i // node_with_edges_frequency) * node_with_edges_frequency + 1
-                upper = lower + node_with_edges_frequency
-                adj_list = ids[lower:upper]
-                info_test_files[out_file_name]['num_edges'] += len(adj_list)
-            else:
-                adj_list = []
-            out_file.write(id + '\t' + version + '\t' + ','.join(adj_list) + '\n')
-        out_file.close()
-
-        # Create a graph where edges occur only between `group_size` contiguous nodes
-        out_file_name = os.path.join(test_files_path, 'test_input_04.txt')
-        out_file = open(out_file_name, 'w')
-        info_test_files[out_file_name] = {}
-        info_test_files[out_file_name]['num_nodes'] = len(ids)
-        info_test_files[out_file_name]['num_edges'] = 0
-        interval = group_size // node_with_edges_frequency
-        for i, id in enumerate(ids):
-            if i % node_with_edges_frequency == 0:
-                lower = (i // group_size) * group_size + 1
-                upper = lower + group_size
-                offset = (i % group_size) // node_with_edges_frequency
-                adj_list = ids[lower + offset:upper:interval]
-                info_test_files[out_file_name]['num_edges'] += len(adj_list)
-            else:
-                adj_list = []
-            out_file.write(id + '\t' + version + '\t' + ','.join(adj_list) + '\n')
-        out_file.close()
-
-        return info_test_files
+            expected = config['expected_test_limited_infection_amount_2019_tolerance_1_percent']
+            self.assertEqual(affected_users, expected)
 
 if __name__ == '__main__':
     unittest.main()
