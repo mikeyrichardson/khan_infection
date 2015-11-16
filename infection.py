de507d4db6cc57917eb3732a4d7b4ecb5d2e8f49
Michael Richardson <michaelrichardson@Severus.local>
2015-11-16 13:30:40 -0800

    Change list to set improve contains check efficiency

--- infection.py
+++ infection.py
@@ -233,25 +233,19 @@ def update_version_for_users(in_file_name, affected_users, version):
     Returns:
         int: The number of updated records.
     """
+    affected_users_set = set(affected_users)
     out_file_name = 'tmp{}'.format(random.randint(10000000,99999999))
     out_file = open(out_file_name, 'w')
     in_file = open(in_file_name)
-    out_lines = []
     for line in in_file:
         fields = line.split('\t')
         userid = fields[0]
-        if userid in affected_users:
-            out_lines.append(userid + '\t' + version + '\t' + '\t'.join(fields[2:]))
+        if userid in affected_users_set:
+            out_file.write(userid + '\t' + version + '\t' + '\t'.join(fields[2:]))
         else:
-            out_lines.append(line)
-        if len(out_lines) > 100000:
-            for line in out_lines:
-                out_file.write(line)
-            out_lines = []
-    for line in out_lines:
-        out_file.write(line)
+            out_file.write(line)
     in_file.close()
     out_file.close()
     os.remove(in_file_name)
     os.rename(out_file_name, in_file_name)
-    return len(affected_users)
+    return len(affected_users_set)
